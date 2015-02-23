4664a39fa9bb0101ab56e0b36167b82eec7d8764
Wongoo Lee <iwongu@gmail.com>
2015-02-23 09:16:24 -0800

    Factor out web and pi related codes.

--- client.js
+++ client.js
@@ -3,96 +3,64 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
-
 var config = JSON.parse(fs.readFileSync('client-config.json'));
-var gpio = config.nogpio ? require('./gpio-debug') : require('./gpio');
+var gpio = config.nogpio ? require('./lib/gpio-debug') : require('./lib/gpio');
 
 var SerialPort = require("serialport").SerialPort
+
+var servers = {};
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var clients = {};
-var servers = {};
+var webclients = require('./lib/webclients')(config, io);
+webclients
+  .on('gpio-write', function(msg) {
+    gpio.write(msg.pin, msg.on);
+  })
+  .on('gpio-read', function(msg) {
+    gpio.read(msg.pin, function(value) {
+      webclients.broadcast('gpio-value', value);
+    });
+  })
+  .on('gpio-direction', function(msg) {
+    gpio.direction(msg.pin, msg.dir, function(value) {
+      webclients.broadcast('gpio-value', value);
+    });
+  })
+  .on('serial-write', function(msg) {
+    serialPort.write(msg, function(err) {
+      if (err) {
+        console.log('err ' + err);
+      }
+    });
+  });
+webclients.serve();
 
-// from browsers.
-io.of('/client')
-  .on('connection', function(socket) {
-    console.log('client connected: ' + socket.id);
-    clients[socket.id] = socket;
-    socket.on('disconnect', function() {
-      console.log('client disconnected: ' + socket.id);
-      delete clients[socket.id];
+var piservers = require('./lib/piservers')(config);
+piservers
+  .on('gpio-write', function(msg) {
+    gpio.write(msg.pin, msg.on);
+  })
+  .on('gpio-direction', function(msg) {
+    gpio.direction(msg.pin, msg.dir, function(value) {
+      piservers.broadcast('gpio-value', value);
     });
-    socket.on('gpio-write', function(msg) {
-      console.log('gpio-write(' + msg.pin + ', ' + msg.on + ')');
-      gpio.write(msg.pin, msg.on);
+  })
+  .on('gpio-read', function(msg) {
+    gpio.read(msg.pin, function(value) {
+      piservers.broadcast('gpio-value', value);
     });
-    socket.on('gpio-read', function(msg) {
-      console.log('gpio-read(' + msg.pin + ')');
-      gpio.read(msg.pin, function(value) {
-        for (client in clients) {
-          clients[client].emit('gpio-value', value);
-        }
-      });
-    });
-    socket.on('gpio-direction', function(msg) {
-      console.log('gpio-direction(' + msg.pin + ', ' + msg.dir + ')');
-      gpio.direction(msg.pin, msg.dir, function(value) {
-        for (client in clients) {
-          clients[client].emit('gpio-value', value);
-        }
-      });
-    });
-    socket.on('serial-write', function(msg) {
-      console.log('serial-write(' + msg + ')');
-      serialPort.write(msg, function(err) {
-        if (err) {
-          console.log('err ' + err);
-        }
-      });
+  })
+  .on('serial-write', function(msg) {
+    serialPort.write(msg, function(err) {
+      if (err) {
+        console.log('err ' + err);
+      }
     });
   });
-
-// from server.
-var socket = require('socket.io-client')(config.server + '/pi');
-socket.on('connect', function() {
-  console.log('connect');
-  servers[socket.id] = socket;
-});
-socket.on('disconnect', function() {
-  console.log('disconnect');
-  delete servers[socket.id];
-});
-socket.on('gpio-write', function(msg) {
-  console.log('gpio-write(' + msg.pin + ', ' + msg.on + ')');
-  gpio.write(msg.pin, msg.on);
-});
-socket.on('gpio-direction', function(msg) {
-  console.log('gpio-direction(' + msg.pin + ', ' + msg.dir + ')');
-  gpio.direction(msg.pin, msg.dir, function(value) {
-    for (server in servers) {
-      servers[server].emit('gpio-value', value);
-    }
-  });
-});
-socket.on('gpio-read', function(msg) {
-  console.log('gpio-read(' + msg.pin + ')');
-  gpio.read(msg.pin, function(value) {
-    for (server in servers) {
-      servers[server].emit('gpio-value', value);
-    }
-  });
-});
-socket.on('serial-write', function(msg) {
-  console.log('serial-write: ' + msg);
-  serialPort.write(msg, function(err) {
-    if (err) {
-      console.log('err ' + err);
-    }
-  });
-});
+piservers.serve();
 
 // for serial port.
 if (config.serial) {
--- server.js
+++ server.js
@@ -10,60 +10,29 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var clients = {};
-var pis = {};
+var webclients = require('./lib/webclients')(config, io);
+var piclients = require('./lib/piclients')(config, io);
 
-// from browsers.
-io.of('/client')
-  .on('connection', function(socket) {
-    console.log('client connected: ' + socket.id);
-    clients[socket.id] = socket;
-    socket.on('disconnect', function() {
-      console.log('client disconnected: ' + socket.id);
-      delete clients[socket.id];
-    });
-    socket.on('gpio-write', function(msg) {
-      console.log('gpio-write(' + msg.pin + ', ' + msg.on + ')');
-      for (pi in pis) {
-        pis[pi].emit('gpio-write', msg);
-      }
-    });
-    socket.on('gpio-read', function(msg) {
-      console.log('gpio-read(' + msg.pin + ')');
-      for (pi in pis) {
-        pis[pi].emit('gpio-read', msg);
-      }
-    });
-    socket.on('gpio-direction', function(msg) {
-      console.log('gpio-direction(' + msg.pin + ', ' + msg.dir + ')');
-      for (pi in pis) {
-        pis[pi].emit('gpio-direction', msg);
-      }
-    });
-    socket.on('serial-write', function(msg) {
-      console.log('serial-write(' + msg + ')');
-      for (pi in pis) {
-        pis[pi].emit('serial-write', msg);
-      }
-    });
+webclients
+  .on('gpio-write', function(msg) {
+    piclients.broadcast('gpio-write', msg);
+  })
+  .on('gpio-read', function(msg) {
+    piclients.broadcast('gpio-read', msg);
+  })
+  .on('gpio-direction', function(msg) {
+    piclients.broadcast('gpio-direction', msg);
+  })
+  .on('serial-write', function(msg) {
+    piclients.broadcast('gpio-write', msg);
   });
+webclients.serve();
 
-// from pi.
-io.of('/pi')
-  .on('connection', function(socket) {
-    console.log('pi connected: ' + socket.id);
-    pis[socket.id] = socket;
-    socket.on('disconnect', function() {
-      console.log('pi disconnected: ' + socket.id);
-      delete pis[socket.id];
-    });
-    socket.on('gpio-value', function(msg) {
-      console.log('gpio-value(' + msg.pin + ', ' + msg.on + ', ' + msg.dir + ')');
-      for (client in clients) {
-        clients[client].emit('gpio-value', msg);
-      }
-    });
-  });
+piclients
+  .on('gpio-value', function(msg) {
+    webclients.broadcast('gpio-value', msg);
+  })
+piclients.serve();
 
 server.listen(config.port, function() {
   console.log('listening on *:' + config.port);
